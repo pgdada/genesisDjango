f63e224311b6fb4927458a30bd1641ba74c5f98f
Promise Dada <gdadapromise@gmail.com>
2021-05-10 23:46:33 -0400

    update blog models

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+from datetime import datetime
+
+class School(models.Model):
+    name = models.CharField(max_length =400)
+    address = models.CharField(max_length = 300)
+    zip_code = models.IntegerField()
+    
+class Student(models.Model):
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length = 50)
+    last_name = models.CharField(max_length = 50)
+    age = models.IntegerField()
+    department = models.CharField(max_length = 50)
+    date_of_resumption = models.DateField(default =datetime.today)
--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # mine
-    # 'blog',
+    'blog',
     'myprofile',
 ]
 
